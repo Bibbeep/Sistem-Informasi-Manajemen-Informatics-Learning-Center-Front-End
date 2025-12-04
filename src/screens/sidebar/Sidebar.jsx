801d2db86430e59ccdff7c39a3274f590975dde2
bibbb <muhammadhabibalfarabi290803@gmail.com>
2025-12-05 01:19:33 +0700

    feat: Add 'My Learnings' navigation item to sidebar.

--- src/screens/sidebar/Sidebar.jsx
+++ src/screens/sidebar/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import {
   FaHome, FaBook, FaComments,
-  FaMoneyBill, FaSignOutAlt
+  FaMoneyBill, FaSignOutAlt, FaFolderOpen // Added FaFolderOpen
 } from 'react-icons/fa';
 import './sidebar.css';
 import { useAuth } from '../../context/AuthContext';
@@ -26,6 +26,9 @@ const Sidebar = () => {
         <div className="sidebar-main-links">
           <NavLink to="/dashboard" className="nav-item" activeclassname="active">
             <FaHome /> Dashboard
+          </NavLink>
+          <NavLink to="/materi" className="nav-item" activeclassname="active">
+            <FaFolderOpen /> My Learnings
           </NavLink>
           <NavLink to="/programs" className="nav-item" activeclassname="active">
             <FaBook /> Programs
